25328ed5b8aafaaba59e3ca21c564ed22339de04
Yuliya Filimonova <yuliya_nova@icloud.com>
2023-08-11 14:11:56 +0600

    Initial Commit

---
+++ CocktailParty/CocktailPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailPartyApp.swift
+//  CocktailParty
+//
+//  Created by Юлия Филимонова on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CocktailPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
